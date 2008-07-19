f34fcb7f8a37614f5fe9cd9e4fa91cf838ee536e
Karel Minarik <karmi@karmi.cz>
2008-07-19 13:21:43 +0200

    Added default route to load Tasks, added better CSS, changed layout to common: app/views/layouts/application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+
   map.resources :tasks
+  
+  map.root :controller => "tasks"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
