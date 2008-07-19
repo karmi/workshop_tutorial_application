cde766c3a061b612e222f4fc94b2af7821225d8e
Karel Minarik <karmi@karmi.cz>
2008-07-19 13:24:10 +0200

    Added validations to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ActiveRecord::Base
+
+  validates_presence_of :name, :due_on
+  
+  # Validates that due date for task cannot be in the past
+  validates_each :due_on, :on => :create do |record, attribute, value| 
+    record.errors.add(attribute, 'date cannot be in the past') if value.to_time < Date.today.to_time
+  end
+  
 end
