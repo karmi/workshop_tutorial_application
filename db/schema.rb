f559aa19d488509aa5cac41a9025dd5bddbaf271
Karel Minarik <karmi@karmi.cz>
2008-07-19 13:32:18 +0200

    Generated blank Project resource
    Command: script/generate scaffold Project name:string description:text

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080719111511) do
+ActiveRecord::Schema.define(:version => 20080719113143) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
